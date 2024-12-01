26f9abdffc4c0f5b074be9744cfb66d272a78f59
Thony <monsivais_th@outlook.com>
2024-11-30 18:47:50 -0600

    Add files via upload

---
+++ potencia.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath> 
+
+// Declaración de la función
+double elevarAPotencia(double base, double exponente);
+
+double elevarAPotencia(double base, double exponente) {
+    return pow(base, exponente); //pow de la biblioteca <cmath>
+}
