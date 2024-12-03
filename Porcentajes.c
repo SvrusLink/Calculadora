3049c2e07d19bdca14a65c17584a0894b300e944
Plastas <81319902+plastaspro@users.noreply.github.com>
2024-12-03 08:13:04 -0600

    Actualizar Porcentajes.c

--- Porcentajes.c
+++ Porcentajes.c
@@ -2,18 +2,9 @@
 
 using namespace std;
 
-void CalcularPorcentaje() {
-    double total, porcentaje;
-
-    // Solicitar los valores al usuario
-    cout << "Ingrese el valor total: ";
-    cin >> total;
-    cout << "Ingrese el porcentaje a calcular: ";
-    cin >> porcentaje;
+double CalcularPorcentaje() {
+    double total, porcentaje;
 
     // Calcular el porcentaje
-    double resultado = (total * porcentaje) / 100.0;
-
-    // Mostrar el resultado
-    cout << "El " << porcentaje << "% de " << total << " es: " << resultado << std::endl;
+    double resultado = (total * porcentaje) / 100.0;
 }
